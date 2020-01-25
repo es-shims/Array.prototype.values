572f6c43372708ea66333b69e34e8103204d025c
Jordan Harband <ljharb@gmail.com>
2020-01-25 10:49:30 -0800

    [Dev Deps] update `@ljharb/eslint-config`

--- index.mjs
+++ index.mjs
@@ -8,7 +8,7 @@ const bound = callBind(getPolyfill());
 export default function values(array) {
 	RequireObjectCoercible(array);
 	return bound(array);
-};
+}
 
 export { default as getPolyfill } from 'array.prototype.values/polyfill';
 export { default as implementation } from 'array.prototype.values/implementation';
