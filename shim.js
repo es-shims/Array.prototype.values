15e4beb96208a02097df86a72569de90dc3824d0
Jordan Harband <ljharb@gmail.com>
2022-04-11 18:23:30 -0700

    [New] `shim`/`auto`: add `values` to `Symbol.unscopables`

--- shim.js
+++ shim.js
@@ -1,14 +1,20 @@
 'use strict';
 
 var define = require('define-properties');
+var shimUnscopables = require('es-shim-unscopables');
+
 var getPolyfill = require('./polyfill');
 
 module.exports = function shimArrayValues() {
 	var polyfill = getPolyfill();
+
 	define(
 		Array.prototype,
 		{ values: polyfill },
 		{ values: function () { return Array.prototype.values !== polyfill; } }
 	);
+
+	shimUnscopables('values');
+
 	return polyfill;
 };
